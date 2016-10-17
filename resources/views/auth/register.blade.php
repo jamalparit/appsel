6d371b8e80a1b67a591c88ee784d7be54d8d10c3
Jamal Parit <jamalparit@outlook.com>
2016-10-18 04:42:52 +0800

    Form
    Untuk membuat form di Laravel kita dapat menggunakan html biasa atau
    menggunakan package [laravelcollective/
    html](https://laravelcollective.com/docs/5.2/html). Menggunakan package,
    proses
    pembuatan form ini akan lebih mudah.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,73 +10,48 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
 
+                        
+                        {!! Form::open(['url'=>'/register', 'class'=>'form-horizontal']) !!}
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
+                            {!! Form::label('name', 'Nama', ['class'=>'col-md-4 control-label']) !!}
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                                {!! Form::text('name', null, ['class'=>'form-control']) !!}
+                                {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
+                            {!! Form::label('email', 'Alamat Email', ['class'=>'col-md-4 control-label']) !!}
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                {!! Form::email('email', null, ['class'=>'form-control']) !!}
+                                {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
+                            {!! Form::label('password', 'Password', ['class'=>'col-md-4 control-label']) !!}
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                {!! Form::password('password', ['class'=>'form-control']) !!}
+                                {!! $errors->first('password', '<p class="help-block">:message</p>') !!}
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
+                            {!! Form::label('password_confirmation', 'Konfirmasi Password', ['class'=>'col-md-4 control-label']) !!}
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
+                                {!! Form::password('password_confirmation', ['class'=>'form-control']) !!}
+                                {!! $errors->first('password_confirmation', '<p class="help-block">:message</p>') !!}
                             </div>
                         </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    <i class="fa fa-btn fa-user"></i> Daftar
                                 </button>
                             </div>
                         </div>
-                    </form>
+                        {!! Form::close() !!}
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Assalamualaikum 
                 </div>
 
                 <div class="links">
