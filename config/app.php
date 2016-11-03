47c95dfe4ecf737a7b84b7b32f8ac5cb1f66718c
Jamal Parit <jamalparit@outlook.com>
2016-11-03 21:30:34 +0800

    Hari 3
    Tamatkan yg tertinggal

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
 
+        Laratrust\LaratrustServiceProvider::class,
+
         //
 
         /*
@@ -225,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Laratrust' => Laratrust\LaratrustFacade::class,
 
     ],
 
